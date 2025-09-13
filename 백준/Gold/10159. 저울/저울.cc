be160867dc4920eab2628da50ec2b8355c5ad956
you1880 <135771445+you1880@users.noreply.github.com>
2025-09-13 23:03:08 +0900

    [Gold IV] Title: 저울, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#define MAX 101
+
+int dist[MAX][MAX];
+int N, M;
+
+void Floyd() {
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dist[i][j] = dist[i][j] | (dist[i][k] & dist[k][j]);
+			}
+		}
+	}
+}
+
+int main() {
+	std::cin.tie(0)->sync_with_stdio(false);
+
+	std::cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i != j) dist[i][j] = 0;
+			else dist[i][j] = 1;
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		std::cin >> a >> b;
+
+		dist[a][b] = 1;
+	}
+	
+	Floyd();
+
+	for (int i = 1; i <= N; i++) {
+		int cnt = 0;
+		for (int j = 1; j <= N; j++) {
+			if (!(dist[i][j] | dist[j][i]))
+				cnt++;
+		}
+
+		std::cout << cnt << "\n";
+	}
+}
